0f574e601c601aebcc4c0ef08195e11c22d4f6af
unknown <natalya_peixoto@hotmail.com>
2019-03-16 11:18:16 -0300

    menu toggle

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const btnMenu = document.querySelector('.nav-burguer')
 const  menu = document.querySelector('.mobile-menu')
 
-btnMenu.addEventListener('click', function(e){
-    e.preventDefault()
+btnMenu.addEventListener('click', function(){  
     menu.classList.toggle('show-menu')
 })
+
+btnMenu.onclick = function(){
+
+}
